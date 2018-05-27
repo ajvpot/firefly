3cc098c0ca1d02b10f4a5d56f2536d72b91717ed
Alex Vanderpot <alexjvanderpotwebdesign@gmail.com>
2018-05-27 14:54:58 -0500

    big config fuck

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,21 @@
 //************************************************************
 #include "painlessMesh.h"
 #include <NeoPixelBus.h>
+#include "main.h"
+#include "FS.h"
 
 #define   MESH_PREFIX      "whateverYouLike"
 #define   MESH_PASSWORD    "somethingSneaky"
 #define   MESH_PORT        5555
 #define   COLOR_SATURATION 128
+#define   CONFIG_FILE       "/config.json"
 
 const uint16_t PixelCount = 50;
 
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
+
+config_t config;         // Current configuration
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
 
@@ -51,12 +56,125 @@ void nodeTimeAdjustedCallback(int32_t offset) {
     Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
 }
 
+
+/////////////////////////////////////////////////////////
+//
+//  JSON / Configuration Section
+//
+/////////////////////////////////////////////////////////
+
+// Configuration Validations TODO
+void validateConfig() {}
+
+// De-Serialize Network config
+void dsConfig(JsonObject &json) {
+  // Generate default hostname if needed
+  config.hostname = json["hostname"].as<String>();
+  if (!config.hostname.length()) {
+    char chipId[7] = {0};
+    snprintf(chipId, sizeof(chipId), "%06x", ESP.getChipId());
+    config.hostname = "fmesh-" + String(chipId);
+  }
+
+	// Fallback to embedded ssid and passphrase if null in config
+	config.ssid = json["ssid"].as<String>();
+	if (!config.ssid.length())
+		config.ssid = "fireflyMesh";
+
+	config.passphrase = json["passphrase"].as<String>();
+	if (!config.passphrase.length())
+		config.passphrase = "fireflyMesh";
+
+  config.gamma = json["gamma"];
+  config.pixelCount = json["pixelCount"];
+}
+
+// Load configugration JSON file
+void loadConfig() {
+	// Zeroize Config struct
+	memset(&config, 0, sizeof(config));
+
+	// Load CONFIG_FILE json. Create and init with defaults if not found
+	File file = SPIFFS.open(CONFIG_FILE, "r");
+	if (!file) {
+		Serial.println(F("- No configuration file found."));
+		config.ssid = "fireflyMesh";
+		config.passphrase = "fireflyMesh";
+		saveConfig();
+	} else {
+		// Parse CONFIG_FILE json
+		size_t size = file.size();
+		if (size > 2048) {
+			Serial.println(F("*** Configuration File too large ***"));
+			return;
+		}
+
+		std::unique_ptr<char[]> buf(new char[size]);
+		file.readBytes(buf.get(), size);
+
+		DynamicJsonBuffer jsonBuffer;
+		JsonObject &json = jsonBuffer.parseObject(buf.get());
+		if (!json.success()) {
+			Serial.println(F("*** Configuration File Format Error ***"));
+			return;
+		}
+
+		dsConfig(json);
+
+		Serial.println(F("- Configuration loaded."));
+	}
+
+	// Validate it
+	validateConfig();
+}
+
+// Serialize the current config into a JSON string
+void serializeConfig(String &jsonString, bool pretty) {
+	// Create buffer and root object
+	DynamicJsonBuffer jsonBuffer;
+	JsonObject &json = jsonBuffer.createObject();
+
+	json["ssid"] = config.ssid.c_str();
+	json["passphrase"] = config.passphrase.c_str();
+	json["hostname"] = config.hostname.c_str();
+
+	json["pixelCount"] = static_cast<uint8_t>(config.pixelCount);
+	json["gamma"] = config.gamma;
+
+	if (pretty)
+		json.prettyPrintTo(jsonString);
+	else
+		json.printTo(jsonString);
+}
+
+// Save configuration JSON file
+void saveConfig() {
+	// Serialize Config
+	String jsonString;
+	serializeConfig(jsonString, true);
+
+	// Save Config
+	File file = SPIFFS.open(CONFIG_FILE, "w");
+	if (!file) {
+		Serial.println(F("*** Error creating configuration file ***"));
+		return;
+	} else {
+		file.println(jsonString);
+		Serial.println(F("* Configuration saved."));
+	}
+}
+
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
   strip.Begin();
   strip.Show();
+
+  SPIFFS.begin();
+  loadConfig();
+  WiFi.hostname(config.hostname);
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
@@ -75,6 +193,7 @@ void loop() {
   userScheduler.execute(); // it will run mesh scheduler as well
   mesh.update();
 
+  //TODO: Replace below with call to animation manager
   strip.ClearTo(RgbColor(COLOR_SATURATION, 0, 0));
   strip.Show();
 }
